546845a94038d9b40b9a736d58971c10c4811cc4
Grove1212 <stella223@korea.ac.kr>
2024-11-09 20:17:49 +0900

    feat(promotion): 해당 상품이 프로모션 기간인지 확인

--- src/main/kotlin/store/Promotion.kt
+++ src/main/kotlin/store/Promotion.kt
@@ -1,20 +1,22 @@
 package store
 
+import camp.nextstep.edu.missionutils.DateTimes
+
 class Promotion(
     private val name: String,
     private val buy: Int,
     private val get: Int,
-    private val start_date: String,
-    private val end_date: String
+    private val startDate: String,
+    private val endDate: String
 ) {
     constructor(line: String) : this(
         name = line.split(",")[0],
         buy = line.split(",")[1].toInt(),
-        get = line.split(",")[1].toInt(),
-        start_date = line.split(",")[2],
-        end_date = line.split(",")[3]
+        get = line.split(",")[2].toInt(),
+        startDate = line.split(",")[3],
+        endDate = line.split(",")[4]
     )
 
     fun isEligibleForPromotion(product: Product) = product.promotion == name
-
+    fun isPromotionOngoing() = DateTimes.now().toString() in startDate..endDate
 }
--- src/test/kotlin/store/PromotionTest.kt
+++ src/test/kotlin/store/PromotionTest.kt
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class PromotionTest {
+    val promotion = Promotion("탄산2+1,2,1,2024-01-01,2024-12-31")
+
     @Test
     fun `Promotion객체가 예외 없이 잘 만들어졌으면 true`() {
         assertDoesNotThrow {
@@ -13,17 +15,18 @@ class PromotionTest {
 
     @Test
     fun `Product가 해당 프로모션 할인 상품인 경우 true 반환`() {
-        val promotion = Promotion("탄산2+1,2,1,2024-01-01,2024-12-31")
         val product = Product("콜라,1000,10,탄산2+1")
         assertEquals(promotion.isEligibleForPromotion(product), true)
     }
 
     @Test
     fun `Product가 해당 프로모션 할인 상품이 아닌 경우 false 반환`() {
-        val promotion = Promotion("탄산2+1,2,1,2024-01-01,2024-12-31")
         val product = Product("콜라,1000,10,null")
         assertEquals(promotion.isEligibleForPromotion(product), false)
     }
 
-    
+    @Test
+    fun `오늘 날짜가 프로모션 기간인 경우 true 반환`(){
+        assertEquals(promotion.isPromotionOngoing(), true)
+    }
 }
