6d875170c14516559fd676a4fcfa0e9186020b8b
Grove1212 <stella223@korea.ac.kr>
2024-11-09 22:31:00 +0900

    feat: 파일을 가져와서 product, promotion 객체들의 리스트 생성

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -4,6 +4,18 @@ import java.io.File
 
 fun main() {
     // TODO: 프로그램 구현
+    //1. 파일 가져오기
+    val PRODUCTSPATH = "/src/main/kotlin/resources/products.md"
+    val PROMOTIONSPATH = "/src/main/kotlin/resources/promotions.md"
+
+    //2. product, promotion 객체 생성
+    val products = File(PRODUCTSPATH).useLines { lines ->
+        lines.map { Product(it) }.toList()
+    }
+    val promotions = File(PROMOTIONSPATH).useLines { lines ->
+        lines.map { Promotion(it) }.toList()
+    }
+
 
 
 }
